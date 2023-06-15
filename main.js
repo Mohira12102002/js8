5b46079d8bec299fad15668a3cacdc53bdf0c585
Mohira Normamatova <mohiranormamatova757@gmail.com>
2023-06-15 09:29:34 +0500

    js8

---
+++ main.js
@@ -0,0 +1,56 @@
+//1.sizga array berilgan. nums = [1,2,3,4]
+// shu arrayni [1,3,6,10] shu shaklda qaytarib bering
+// Tushuntirish: [1, 1+2, 1+2+3, 1+2+3+4].
+
+// Tushuntirish: nums = [1,1,1,1,1] => [1,2,3,4,5] Sababi: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1]
+
+// Tushuntirish: nums = [3,1,2,10,1] => [3,4,6,16,17]
+
+
+let nums = [1,1,1,1,1];
+
+
+    let result = [];
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+      total += nums[i];
+      result.push(total);
+    }
+    console.log(result);
+  
+  
+
+//2.Gaplardan iborat array berilgan. shu gaplar orasidan eng uzun gapdagi so'zlar sonini chiqazing 
+// ["alice and bob love google", "i think so too", "this is great thanks very much"]
+// "alice and bob love google" => 5 ta so'z 
+//  "i think so too" => 4 ta so'z
+// "this is great thanks very much" => 6 ta so'z
+
+const sentences = ["alice and bob love google", "i think so too", "this is great thanks very much"];
+
+
+    let maxWordCount = 0;
+    for (let i = 0; i < sentences.length; i++) {
+      const words = sentences[i].split(' ');
+      const wordCount = words.length;
+      if (wordCount > maxWordCount) {
+        maxWordCount = wordCount;
+      }
+    }
+    console.log(maxWordCount); 
+  
+
+
+//
+
+const gap = "Hello how are you Contestant";
+const son = 4;
+const words = gap.split(' ');
+const shortenedWords = words.slice(0, son);
+const shortenedSentence = shortenedWords.join(' ');
+console.log(shortenedSentence); 
+
+    
+  
+  
+  
